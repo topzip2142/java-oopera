15a1a404b18e6b292c44fbd873b5270e47a7b748
topzip <topzip2142@yandex.ru>
2026-01-13 10:35:43 +0300

    Добавлены необходимые импорты, проверка метода у класса Director

--- src/Theatre.java
+++ src/Theatre.java
@@ -1,6 +1,10 @@
+import theatre.Director;
+import theatre.Gender;
+
 public class Theatre {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+        Director director = new Director("Иван", "Иванов", Gender.MALE, 3);
+        System.out.println(director.toString());
     }
 }
