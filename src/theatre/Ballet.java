274d2063c7d30ef3df07d1fef83820e44c1dce50
topzip <topzip2142@yandex.ru>
2026-01-13 09:48:06 +0300

    Добавлен класс

---
+++ src/theatre/Ballet.java
@@ -0,0 +1,10 @@
+package theatre;
+
+public class Ballet extends MusicalShow {
+    private Person choreographer;
+
+    public Ballet(String title, int duration, Director director, Person musicAuthor, String librettoText, Person choreographer) {
+        super(title, duration, director, musicAuthor, librettoText);
+        this.choreographer = choreographer;
+    }
+}
