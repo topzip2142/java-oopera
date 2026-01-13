499bf8d6693d7e1a1f0237a4eecdcabb72f3a05d
topzip <topzip2142@yandex.ru>
2026-01-13 09:33:17 +0300

    Добавлены основные классы

---
+++ src/theatre/Director.java
@@ -0,0 +1,10 @@
+package theatre;
+
+public class Director extends Person {
+    private int numberOfShows;
+
+    public Director(String name, String surname, Gender gender, int numberOfShows) {
+        super(name, surname, gender);
+        this.numberOfShows = numberOfShows;
+    }
+}
---
+++ src/theatre/Show.java
@@ -0,0 +1,10 @@
+package theatre;
+
+import java.util.ArrayList;
+
+public class Show {
+    private int duration;
+    private String title;
+    private Director director;
+    private ArrayList<Actor> listOfActors;
+}
