21348d912ce325a38e6b331c18bcd59077c201de
topzip <topzip2142@yandex.ru>
2026-01-14 21:51:11 +0300

    Добавлены основные методы печати списка актёров, добавления актёра и замены актёра

--- src/theatre/show/Show.java
+++ src/theatre/show/Show.java
@@ -6,14 +6,72 @@ import theatre.staff.Director;
 import java.util.ArrayList;
 
 public class Show {
-    private String title;
-    private int duration;
-    private Director director;
-    private ArrayList<Actor> listOfActors;
+    protected String title;
+    protected int duration;
+    protected Director director;
+    protected ArrayList<Actor> listOfActors;
+    protected static final String ERROR_NO_ACTORS = "В данном спектакле никто не участвует!";
 
     public Show(String title, int duration, Director director) {
         this.title = title;
         this.duration = duration;
         this.director = director;
+    }
+
+    public void printActors() {
+        if (listOfActors.isEmpty()) {
+            System.out.println(ERROR_NO_ACTORS);
+            return;
+        }
+
+        System.out.println("Список актёров, участвующих в спектакле:");
+
+        for (Actor actor : listOfActors) {
+            System.out.println(" " + actor);
+        }
+    }
+    
+    public void addActor(Actor newActor) {
+        if (listOfActors.isEmpty()) {
+            listOfActors.add(newActor);
+        } else {
+            for (Actor actor : listOfActors) {
+                if (actor.equals(newActor)) {
+                    System.out.println("Такой актёр уже участвует в спектакле!");
+                    return;
+                }
+            }
+            
+            listOfActors.add(newActor);
+            System.out.println(newActor + " теперь участвует в спектакле");
+        }
+    }
+    
+    public void changeActor(Actor newActor, String surnameToChange) {
+        if (listOfActors.isEmpty()) {
+            System.out.println(ERROR_NO_ACTORS);
+            return;
+        }
+
+        boolean isActorInShow = false;
+        for (Actor actor : listOfActors) {
+            if (actor.getSurname().equals(surnameToChange)) {
+                isActorInShow = true;
+                break;
+            }
+        }
+
+        if (isActorInShow) {
+            for (int i = 0; i < listOfActors.size(); i++) {
+                Actor actor = listOfActors.get(i);
+                if (actor.getSurname().equals(surnameToChange)) {
+                    listOfActors.remove(i);
+                    listOfActors.add(newActor);
+                    break;
+                }
+            }
+        } else {
+            System.out.println("Актёра с фамилией '" + surnameToChange + "' нет в данном спектакле");
+        }
     }
 }
