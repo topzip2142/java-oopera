831f6be7854d11e2be8f95c936a5c8fea20e736a
topzip <topzip2142@yandex.ru>
2026-01-14 18:43:23 +0300

    Добавлены переопределения методотов сравнения и хэшкода

--- src/theatre/staff/Actor.java
+++ src/theatre/staff/Actor.java
@@ -1,10 +1,43 @@
 package theatre.staff;
 
+import java.util.Objects;
+
 public class Actor extends Person {
-    private int height;
+    private final int height;
+    private static final int HASH_SIMPLE_NUMBER_ONE = 17;
+    private static final int HASH_SIMPLE_NUMBER_TWO = 31;
 
     public Actor(String name, String surname, Gender gender, int height) {
         super(name, surname, gender);
         this.height = height;
+    }
+
+    @Override
+    public int hashCode() {
+
+        int hash = HASH_SIMPLE_NUMBER_ONE;
+
+        hash = hash + name.hashCode();
+        hash = hash * HASH_SIMPLE_NUMBER_TWO;
+        hash = hash + surname.hashCode();
+        hash = hash * HASH_SIMPLE_NUMBER_TWO;
+        hash = hash + gender.hashCode();
+        hash = hash * HASH_SIMPLE_NUMBER_TWO;
+        hash = hash + height;
+
+        return hash;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Actor anotherActor = (Actor) obj;
+
+        return  Objects.equals(name, anotherActor.name) &&
+                Objects.equals(surname, anotherActor.surname) &&
+                Objects.equals(gender, anotherActor.gender) &&
+                Objects.equals(height, anotherActor.height);
     }
 }
