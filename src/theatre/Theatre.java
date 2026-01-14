c520566aeb319139c125869802cb064b2054f1c3
topzip <topzip2142@yandex.ru>
2026-01-15 02:23:36 +0300

    Добавление данных, согласно ТЗ

--- src/theatre/Theatre.java
+++ src/theatre/Theatre.java
@@ -1,12 +1,62 @@
 package theatre;
 
-import theatre.staff.Director;
-import theatre.staff.Gender;
+import theatre.staff.*;
+import theatre.show.*;
 
 public class Theatre {
 
     public static void main(String[] args) {
-        Director director = new Director("Иван", "Иванов", Gender.MALE, 3);
-        System.out.println(director);
+        Actor actorPetrov = new Actor("Александр", "Петров", Gender.MALE, 178);
+        Actor actorHodchenkova = new Actor("Светлана", "Ходченкова", Gender.FEMALE, 179);
+        Actor actorRedcliff = new Actor("Даниэль", "Рэдклифф", Gender.MALE, 165);
+
+        Director directorTuminas = new Director("Римас", "Туминас", Gender.MALE, 30);
+        Director directorChusova = new Director("Нина", "Чусова", Gender.FEMALE, 40);
+
+        Person musicAuthorZimmer = new Person("Ханс", "Циммер", Gender.MALE);
+        Person choreographerGrigorovich = new Person("Юрий", "Григорович", Gender.MALE);
+
+        String librettoOfBallet = "Она ищет себя в мире теней и воспоминаний; Он воплощает неумолимое время; "
+                                + "их дуэты — борьба и притяжение, а финальный танец с «серебряной нитью» "
+                                + "символизирует примирение души со временем и принятие прошлого.";
+        String librettoOfMusicalShow = "Лия мечтает о сцене, но её голос тонет в шуме «голосов»"
+                                     + "соцсетей; с помощью друга Макса и через противостояние"
+                                     + " мистическому Эхо она находит силу в собственной искренности, "
+                                     + " завершая шоу манифестом: «Мой голос — мой выбор», — при поддержке всех "
+                                     + "персонажей и зрителей.";
+
+        Show show = new Show("Часы без стрелок", 105, directorTuminas);
+
+        Ballet ballet = new Ballet("Серебряная нить", 90,
+                                    directorChusova, musicAuthorZimmer,
+                                    librettoOfBallet, choreographerGrigorovich);
+
+        Opera opera = new Opera("Громче, чем эхо!", 120,
+                                directorChusova, musicAuthorZimmer,
+                                librettoOfMusicalShow, 25);
+
+        show.addActor(actorPetrov);
+        show.addActor(actorPetrov);
+        show.addActor(actorRedcliff);
+
+        ballet.addActor(actorPetrov);
+        ballet.addActor(actorHodchenkova);
+
+        opera.addActor(actorPetrov);
+        opera.addActor(actorHodchenkova);
+        opera.addActor(actorRedcliff);
+
+        show.printActors();
+        ballet.printActors();
+        opera.printActors();
+
+        show.changeActor(actorHodchenkova, "Рэдклифф");
+        show.printActors();
+
+        opera.changeActor(actorPetrov, "Грин");
+
+        ballet.printLibrettoText();
+        opera.printLibrettoText();
+
     }
 }
