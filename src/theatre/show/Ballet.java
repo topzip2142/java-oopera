f196bd3931be2466a9c623f4d1f6ffb99a8476d1
topzip <topzip2142@yandex.ru>
2026-01-15 02:22:24 +0300

    Реформат кода

--- src/theatre/show/Ballet.java
+++ src/theatre/show/Ballet.java
@@ -6,7 +6,8 @@ import theatre.staff.Person;
 public class Ballet extends MusicalShow {
     private Person choreographer;
 
-    public Ballet(String title, int duration, Director director, Person musicAuthor, String librettoText, Person choreographer) {
+    public Ballet(String title, int duration, Director director, Person musicAuthor,
+                  String librettoText, Person choreographer) {
         super(title, duration, director, musicAuthor, librettoText);
         this.choreographer = choreographer;
     }
